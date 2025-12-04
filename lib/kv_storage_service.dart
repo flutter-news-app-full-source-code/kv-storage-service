5905bd09a238c81291e43c0957a07efbca1fa256
fulleni <fulleni@hotmail.com>
2025-12-04 03:48:46 +0100

    refactor(kv_storage_service): move exceptions to separate file
    - Created new file lib/src/exceptions.dart for storage-related exceptions
    - Updated lib/kv_storage_service.dart to export the new exceptions file
    - Moved all exception classes from lib/src/kv_storage_service.dart to the new file
    - Updated imports in lib/src/kv_storage_service.dart to reflect the changes

--- lib/kv_storage_service.dart
+++ lib/kv_storage_service.dart
@@ -1,5 +1,6 @@
 /// Defines an abstract interface for key-value storage services.
 library kv_storage_service;
 
+export 'src/exceptions.dart';
 export 'src/kv_storage_service.dart';
 export 'src/storage_keys.dart';
